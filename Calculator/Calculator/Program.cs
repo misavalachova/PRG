4525662877e01c5cf877b30c0748c2a71c3f0d63
Michaela Valachova <Misa62v@gmail.com>
2024-10-13 23:57:51 +0200

    Update Calculator

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -34,59 +34,84 @@ namespace Calculator
              *       - https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/statements/iteration-statements#the-while-statement
              * 3) Umozni uzivateli zadavat i desetinna cisla, tedy prekopej kalkulacku tak, aby umela pracovat s floaty
              */
-            float first = LoadNumber();
-            float second = LoadNumber();
-            float result = Calculate(first, second);
-             
-            
-            Console.WriteLine("Vysledek je " +result);
 
 
 
 
 
+
+            float first = LoadNumber();//Povolame funkci napsanou nize 'LoadNumber', nacita vstup (zadane cislo) od uzivatele
+            float second = LoadNumber(); //To same provadi s druhou promennou
+            float result = Calculate(first, second); //Povolame funkci Calculate a uvedeme cisla s kterymi pracujeme
+            Console.WriteLine("Vysledek je " + result); //Nyni uz jen vypise vysledek do konzole
             Console.ReadKey(); //Toto nech jako posledni radek, aby se program neukoncil ihned, ale cekal na stisk klavesy od uzivatele.
         }
         static float LoadNumber()
         {
-            Console.WriteLine("zadej číslo");
-            float number = float.Parse(Console.ReadLine());
-            return number;
+            Console.WriteLine("zadej číslo"); //Do konzole se vypisou instrukce uzivateli co ma zadat
+            float number; //Zavedeme si novou promennou float
+            while (!float.TryParse(Console.ReadLine(), out number)) //Timto overujeme, zda uzivatel nezada neplatnou hodnotu
+            {
+                Console.WriteLine("Neplatne cislo, zadej prosim znovu"); 
+            }   
+            return number; //vraceni promenne
         }
-        static float Calculate(float firstNumber, float secondNumber)
+        static float Calculate(float firstNumber, float secondNumber) //Vytvorime si funkci 'Calculate' pro operace, ktere budeme provadet se zadanymi cisly, do zavorek musime zadat dva nami pojmenovane parametry
         {
-            Console.WriteLine("Nyni napis, kterou operaci chces provest: soucet, rozdil, soucin, podil, mocneni, odmocneni");
-            string input = Console.ReadLine();
-            float result = 0;
-            if (input == "soucet")
-            {
-                result = firstNumber + secondNumber;
+            Console.WriteLine("Nyni napis, kterou operaci chces provest: soucet, rozdil, soucin, podil, mocneni, odmocneni cisla 1, odmocneni cisla 2");
+            string input = Console.ReadLine(); //Nacteme vstup od uzivatele, tedy zadanou operaci podle instrukci
+            float result = 0; //promenna s vysledkem, prozatim priradime hodnotu nula
+            switch (input) // pouzijeme podminku, abychom rozeznali zadane operace
+            { //switch narozdil of
+                case "soucet": // v pripade, ze uzivatel napise soucet
+                    result = firstNumber + secondNumber;
+                    break; //ukonceni pripadu
+                case "rozdil":
+                    result = firstNumber - secondNumber;
+                    break;
+                case "soucin":
+                    result = firstNumber * secondNumber;
+                    break;
+                case "podil":
+                    result = firstNumber / secondNumber;
+                    break;
+                case "mocneni":
+                    result = (float)Math.Pow((double)firstNumber, (double)secondNumber); //Vypocita prvni cislo umocnene na druhe zadane cislo
+                    break;
+                case "odmocneni cisla 1":
+                    
+                    
+                        if (firstNumber < 0)
+                        {
+                            Console.WriteLine("Ze zaporneho cisla nelze spocitat odmocninu");
+                        }
+                        else
+                        {
+                            result = (float)Math.Sqrt((double)firstNumber);
+                        }
+                    break;
+                case "odmocneni cisla 2":
+                    if (secondNumber < 0)
+                    {
+                        Console.WriteLine("Ze zaporneho cisla nelze spocitat odmocninu");
+                    }
+                    else
+                    {
+                        result = (float)Math.Sqrt((double)secondNumber);
+                    }
+                    break;
+                default: //pokud se zadana hodnota od uzivatele neshoduje s zadnou z vyjmenovanych, povazuje se za spatne zadanou a zada se znovu
+                    Console.WriteLine("Spatne zadana operace, prosim napis ji znovu");
+
+                    break;
             }
-            if (input == "rozdil")
-            {
-                result = firstNumber - secondNumber;
-            }
-            if (input == "soucin")
-            {
-                result = firstNumber * secondNumber;
-            }
-            if (input == "podil")
-            {
-                result = firstNumber / secondNumber;
-            }
-            if (input == "mocneni")
-            {
-                result = (float)Math.Pow((double)firstNumber, (double)secondNumber);
-            }
-            if (input == "odmocneni")
-            {
-                result = (float)Math.Sqrt((double)firstNumber);
-            }
-            else
-            {
-                Console.WriteLine("Nebyla zadana spravna operace, prosim napis ji znovu");
-            }
-            return result;
+            
+            return result; //vrati vyslednou hodnotu, ktera se pote pouzije v main
+
+
+
         }
+        
+
     }
 }
